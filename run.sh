19e5fbffa26d9231708dd5d5c87f586a9abc9807
truerexdy <jnani@duck.com>
2025-09-14 08:56:32 +0530

    fixed

--- run.sh
+++ run.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-sudo apt update
-echo "Installing Dependencies"
-sudo apt install -y podman podman-compose golang
-echo "Setting up dir structure"
-chmod +x deployment/dir_setup.sh
-./deployment/dir_setup.sh
-echo "Starting Containers"
-cd deployment
-podman-compose up -d
-cd ..
-echo "Cloning Repo:"
-cd ./public
-git clone https://github.com/Chiranthcs6/Website.git
-cd ..
-sleep 10
-echo "Starting the app"
-go run ./cmd/main.go
-echo "App stopped"
+cd ./public/
+git pull
+cd ../cmd/
+go run ./main.go
---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euo pipefail
+sudo apt update
+echo "Installing Dependencies"
+sudo apt install -y podman podman-compose golang
+echo "Setting up dir structure"
+chmod +x deployment/dir_setup.sh
+./deployment/dir_setup.sh
+echo "Starting Containers"
+cd deployment
+podman-compose up -d
+cd ..
+echo "Cloning Repo:"
+mkdir -p public
+cd ./public
+git clone https://github.com/Chiranthcs6/Website.git
+mv Website/* Website/.* ./
+rmdir Website
+cd ..
+echo "Setup Complete, now do bash run.sh to start the server."
