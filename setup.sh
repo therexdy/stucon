16042ed87c429fc34b1649be64e44b4af047bec4
truerexdy <jnani@duck.com>
2025-09-26 05:31:09 +0530

    min

--- setup.sh
+++ setup.sh
@@ -5,13 +5,14 @@ echo "Installing Dependencies"
 # install -y podman podman-compose 
 echo "Setting up dir structure"
 chmod +x deployment/dir_setup.sh
-./deployment/dir_setup.sh
+cd ./deployment
+./dir_setup.sh
 echo "Cloning Repo:"
+cd ../
 mkdir -p public
 cd ./public
 git clone https://github.com/Chiranthcs6/stucon_Connect.git
 repo_name="stucon_Connect"
 mv $repo_name/* $repo_name/.* ./
 rmdir $repo_name
-cd ..
 echo "Setup Complete, now do bash run.sh to start the server."
